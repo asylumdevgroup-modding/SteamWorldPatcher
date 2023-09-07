8f861dc73e3df560713dbe2a698602155f1289b1
mcadventurecity <legodata@comcast.net>
2023-09-06 20:54:08 -0400

    Replace Inject with Overwrite
    Injecting wasn't working in ADG Omega 1

--- src/main/java/com/asylumdev/steamworldpatcher/SteamWorldPatcher.java
+++ src/main/java/com/asylumdev/steamworldpatcher/SteamWorldPatcher.java
@@ -2,7 +2,7 @@ package com.asylumdev.steamworldpatcher;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid="steamworldpatcher", name="SteamWorldPatcher",version="1.0")
+@Mod(modid="steamworldpatcher", name="SteamWorldPatcher",version="1.1",dependencies = SteamWorldPatcher.DEPENDENCIES)
 public class SteamWorldPatcher {
     public static final String DEPENDENCIES = "required-after:mixinbooter@[8.0,);required-after:steamworld;";
 
--- src/main/java/com/asylumdev/steamworldpatcher/mixin/OldSkyMixin.java
+++ src/main/java/com/asylumdev/steamworldpatcher/mixin/OldSkyMixin.java
@@ -3,6 +3,7 @@ package com.asylumdev.steamworldpatcher.mixin;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeProvider;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -10,9 +11,13 @@ import zaexides.steamworld.world.dimension.BiomeProviderSkyOfOld;
 
 @Mixin(value= BiomeProviderSkyOfOld.class)
 public class OldSkyMixin extends BiomeProvider {
-
-    @Inject(method="getBiomes", at= @At("RETURN"), cancellable=true)
-    private void patchGetBiomes(Biome[] oldBiomeList, int x, int z, int width, int depth, CallbackInfoReturnable<Biome[]> cir) {
-        cir.setReturnValue(super.getBiomes(oldBiomeList,x,z,width,depth));
+    
+    /**
+     * @author MCAdventureCity
+     * @reason Injector wasn't working.
+     */
+    @Overwrite
+    public Biome[] getBiomes(Biome[] oldBiomeList, int x, int z, int width, int depth) {
+        return super.getBiomes(oldBiomeList, x, z, width, depth);
     }
 }
